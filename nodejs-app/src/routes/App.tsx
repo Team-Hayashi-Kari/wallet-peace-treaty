df9c173cf5b06d5eeee130627b05ece4fe71ad00
Yuu0413s <y.shibata0820@gmail.com>
2025-07-02 07:49:16 +0900

    💄 電卓画面の更新

--- nodejs-app/src/routes/App.tsx
+++ nodejs-app/src/routes/App.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import { createFileRoute } from "@tanstack/react-router"
+import React, { useState, useMemo } from 'react';
 
 import "./App.css";
 import NumBtn from "./components/NumBtn";
 import OperatorBtn from "./components/OperatorBtn";
 import Result from "./components/Result";
+import Context from "./components/Context";
+import {
+onClearClick,
+onEqualClick,
+onDivideClick,
+onMultiplyClick,
+onPlusClick,
+onMinusClick
+} from "./reducers";
 
 const App: React.FC = () => {
+const { state } = useContext(Context);
+
 return (
     <>
     <div className="result">
-        <Result result={"計算結果"} />
+        <Result
+        result={state.showingResult ? state.resultValue : state.inputValue}
+        />
     </div>
     <div className="wrapper">
         <div className="number">
@@ -31,22 +45,24 @@ return (
         <div className="zero">
             <NumBtn n={0} />
             <span className="allClear">
-            <OperatorBtn o={"AC"} />
+            <OperatorBtn o={"AC"} action={onClearClick()} />
             </span>
             <span className="equal">
-            <OperatorBtn o={"="} />
+            <OperatorBtn o={"="} action={onEqualClick()} />
             </span>
         </div>
         </div>
         <div className="operator">
-        <OperatorBtn o={"÷"} />
-        <OperatorBtn o={"×"} />
-        <OperatorBtn o={"-"} />
-        <OperatorBtn o={"+"} />
+        <OperatorBtn o={"÷"} action={onDivideClick()} />
+        <OperatorBtn o={"×"} action={onMultiplyClick()} />
+        <OperatorBtn o={"-"} action={onMinusClick()} />
+        <OperatorBtn o={"+"} action={onPlusClick()} />
         </div>
     </div>
     </>
 );
 };
 
-export default App;
+export default CalculatorTabs;
+
+export const Route = createFileRoute('/calc')({component: CalculatorTabs,});
