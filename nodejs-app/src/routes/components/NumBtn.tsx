7ddbbb028be5bd05ae1532eb3bf9031f3ff37c26
Yuu0413s <y.shibata0820@gmail.com>
2025-07-02 07:32:50 +0900

    🎨 componentsファイルを作成

---
+++ nodejs-app/src/routes/components/NumBtn.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from "react";
+
+const NumBtn: FC<{ n: number }> = ({ n }) => <button>{n}</button>;
+
+export default NumBtn;
---
+++ nodejs-app/src/routes/components/OperatorBtn.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from "react";
+
+const OperatorBtn: FC<{ o: string }> = ({ o }) => <button>{o}</button>;
+
+export default OperatorBtn;
