7a850ccb5e386c29ba0356a221f156cb2b36a659
0yaizu <kaira.oyaizu@gmail.com>
2025-07-04 14:28:49 +0900

    💄 一番最初のタイトル部分を作成

--- nodejs-app/src/routes/index.tsx
+++ nodejs-app/src/routes/index.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { createFileRoute } from '@tanstack/react-router'
+import { Box, Button, Flex, Heading, HStack, Image, VStack } from '@yamada-ui/react'
 // import { Button } from '@yamada-ui/react';
 // import { FontAwesomeIcon } from "@yamada-ui/fontawesome"
 // import { faPoo } from "@fortawesome/free-solid-svg-icons"
@@ -13,50 +14,6 @@ export const Route = createFileRoute('/')({
 })
 
 const styles = {
-  container: css({
-    maxWidth: '1200px',
-    margin: '0 auto',
-    padding: '2rem'
-  }),
-  headingWrapper: css({
-    background: 'linear-gradient(45deg, #FFC973, #FF7D29)', 
-    marginBottom: '1rem',
-    padding: '2rem',
-    position: 'relative',
-    minHeight: '500px',
-    overflow: 'hidden',
-  }),
-  title: css({
-    color: '#1a1a1a',
-    fontSize: '3.5rem',
-    position: 'absolute', 
-    top: '35%',
-    left: '100px',
-    whiteSpace: 'nowrap',
-
-    '@media (max-width: 768px)': { // 画面幅が768px以下の時に適用
-      fontSize: '2.5rem', // フォントサイズを小さくする
-      left: '50px', // 左からの位置も調整
-      transform: 'translateX(0)', // 固定ピクセル値なのでtransformは不要になることが多い
-      whiteSpace: 'normal', // 必要であれば折り返しを許可
-    },
-  }),
-  subTitle: css({
-    color: '#1a1a1a',
-    fontSize: '1.2rem',
-    position: 'absolute',
-    top: '55%',
-    left: '150px',
-    textAlign: 'center',
-    whiteSpace: 'nowrap',
-
-    '@media (max-width: 768px)': {
-      fontSize: '0.9rem',
-      top: '50%',
-      left: '80px',
-      whiteSpace: 'normal',
-    },
-  }),
   explanation: css({
     color: '#1a1a1a',
     fontSize: '0.9rem',
@@ -77,45 +34,25 @@ const styles = {
     '@media (max-width: 768px)': {
       fontSize: '0.55rem',
     },
-  }),
-  button: css({
-    position: 'absolute',
-    top: '55%',
-    left: '150px',
-    textAlign: 'center',
-    whiteSpace: 'nowrap',
   })
 }
 
+const titleStyle = css({
+	background: "linear-gradient(45deg,rgba(255, 201, 115, 1) 0%, rgba(255, 125, 41, 1) 100%)"
+})
+
 function TopPage() {
   return (
-    <>
-      <div css={styles.headingWrapper}>
-        <h1 css={styles.title}>お財布平和条約</h1>
-        <h3 css={styles.subTitle}>割り勘をスムーズに行うツール</h3>
-
-      {/* <div>
-        <Button
-          colorScheme="orange"
-          size="lg"         
-          variant="solid"   
-          borderRadius="full"
-          onClick={() => alert('割り勘を始めましょう！')}
-          // その他のプロパティも多数あります
-          // 例えば、width="200px" や isLoading={true} など
-        >
-          割り勘を始める
-        </Button>
-      </div> */}
-        <div css={styles.button}>
-          {/* <button css={styles.button} onClick={() => alert('割り勘を始めましょう！')}>
-            割り勘を始める
-          </button> */}
-          {/* <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p> */}
-        </div>
-      </div>
+    <Box>
+			<HStack bg={'orange.300'} p="lg" height={'100vh'} justifyContent={'space-evenly'} css={titleStyle}>
+				<VStack color={'white'} textAlign={'center'} p="lg" alignItems="center" width="fit-content">
+					<Heading as={'h1'}>お財布平和条約</Heading>
+					<Box whiteSpace={'nowrap'}>割り勘、後から清算するのをスムーズに</Box>
+					<Button variant="outline" colorScheme="orange" >割り勘を始める</Button>
+				</VStack>
+				<Image src='https://soco-st.com/wp-content/themes/socost/upload/8194_line.svg' boxSize='lg' minBoxSize='sm' alt="SVG Image" fallback="https://placehold.co/384" display={{ base: 'block', md: 'none' }} />
+			</HStack>
+			{/* 説明部分 */}
       <div css={styles.explanation}>
         <h2>みんなとご飯を食べたとき、割り勘するの大変じゃないですか？</h2>
         <h2>そんな悩み、解決します</h2>
@@ -123,6 +60,6 @@ function TopPage() {
       <p css={styles.readTheDocs}>
         {/* Click on the Vite and React logos to learn more */}
       </p>
-    </>
+    </Box>
   )
 }
