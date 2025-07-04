b4e37cd3bf86bc67245b39287b55b5d448c0a4e7
Yuu0413s <y.shibata0820@gmail.com>
2025-07-04 16:03:45 +0900

    🚧 PaymentsPage.tsxを作成・記述

---
+++ nodejs-app/src/pages/PaymentsPage.tsx
@@ -0,0 +1,28 @@
+import {Box, Heading, VStack, Divider} from "@yamada-ui/react";
+
+import {usePayments} from '../hooks/usePayments';
+import {PaymentForm} from '../components/payments/PaymentForm';
+import {PaymentList} from '../components/payments/PaymentList';
+
+export const PaymentPages = () => {
+    const {payments, addPayment, updatePayment, deletePayment} = usePayments();
+
+    return(
+        <Box maxW="container.lg" mx="auto" p="md">
+            <VStack spacing="lg" align="stretch">
+                <Heading as="h1" size="x1" textAlign="center">
+                    精算管理モード
+                </Heading>
+
+                <PaymentForm addPayment=(addPayment) />
+                <Divider />
+
+                <PaymentList
+                payments={payments}
+                updatePayment={updatePayment}
+                deletePayment={deletePayment}
+                />
+            </VStack>
+        </Box>
+    )
+}
