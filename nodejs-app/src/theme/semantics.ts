6c3e3cb47e3f5f49ceee917bd6d67b18d17278b2
0yaizu <kaira.oyaizu@gmail.com>
2025-07-05 09:27:39 +0900

    💄 グローバルスタイルを指定

---
+++ nodejs-app/src/theme/semantics.ts
@@ -0,0 +1,33 @@
+import type { ThemeSemantics } from "@yamada-ui/react"
+
+export const semantics: ThemeSemantics = {
+  colors: {
+    primary: "blue.500",
+    secondary: "violet.500",
+    info: "blue.500",
+    success: "green.500",
+    warning: "orange.500",
+    danger: "red.500",
+    link: "blue.500",
+  },
+  colorSchemes: {
+    primary: "blue",
+    secondary: "violet",
+    info: "blue",
+    success: "green",
+    warning: "orange",
+    danger: "red",
+    link: "blue",
+  },
+  spaces: {
+    xs: "1",
+    sm: "2",
+    md: "4",
+    normal: "6",
+    lg: "8",
+    xl: "12",
+    "2xl": "16",
+    "3xl": "24",
+    "4xl": "32",
+  },
+}
